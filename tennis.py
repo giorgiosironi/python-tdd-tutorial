31427deab91689e9ed9967b911b206409908df65
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-22 10:38:17 +0100

    Separated score counting from naming

--- tennis.py
+++ tennis.py
@@ -1,8 +1,14 @@
 class Set:
     def __init__(self):
         self.first = 0
-        self.scoreNames = ["0", "15", "30", "40"]
+        self.scores = Scores()
     def firstScores(self):
         self.first = self.first + 1
     def firstScore(self):
-        return self.scoreNames[self.first]
+        return self.scores.scoreName(self.first)
+
+class Scores:
+    def __init__(self):
+        self.scoreNames = ["0", "15", "30", "40", "A"]
+    def scoreName(self, index):
+        return self.scoreNames[index]
--- test_tennis.py
+++ test_tennis.py
@@ -1,13 +1,18 @@
-from tennis import Set
+from tennis import Set, Scores
 from unittest import TestCase
 
 class TestSetWinning(TestCase):
-    def test_score_names(self):
+    def test_score_grows(self):
         set = Set()
         self.assertEqual("0", set.firstScore())
         set.firstScores()
         self.assertEqual("15", set.firstScore())
-        set.firstScores()
-        self.assertEqual("30", set.firstScore())
-        set.firstScores()
-        self.assertEqual("40", set.firstScore())
+
+class TestScoreNames(TestCase):
+    def test_score_names(self):
+        scores = Scores()
+        self.assertEqual("0", scores.scoreName(0))
+        self.assertEqual("15", scores.scoreName(1))
+        self.assertEqual("30", scores.scoreName(2))
+        self.assertEqual("40", scores.scoreName(3))
+        self.assertEqual("A", scores.scoreName(4))
